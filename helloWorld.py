8d20606afff0fb1b2b5d3a1cc6bf3957cf30396b
codebungalow <tcm@amazon.com>
2018-11-27 17:00:05 -0800

    Added message indicating its from tester2 branch

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,6 @@
-print "Hello Test Branch"
-print "First Modification"
+print ("Hello Test Branch")
+print ("First Modification")
+print ("Creaed in branch: tester2")
 
 for x in range(6):
   print("count: ", x)
